488d5079b7583b922bfa47aa5e6938b2f9b51e50
Selva <mrnskselva@gmail.com>
2025-01-26 22:23:35 +0530

    added padding

--- my-react-app/src/components/Pagination.jsx
+++ my-react-app/src/components/Pagination.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Pagination = ({ currentPage, totalPages, onPrevPage, onNextPage, onPageSelect }) => {
     return (
-        <nav className="flex items-center gap-x-1" aria-label="Pagination">
+        <nav className="flex items-center gap-x-1 pb-10" aria-label="Pagination">
             <button type="button" className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10" aria-label="Previous" onClick={onPrevPage} disabled={currentPage === 1}>
                 <svg className="shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                     <path d="m15 18-6-6 6-6"></path>
