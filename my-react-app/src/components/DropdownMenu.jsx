52a1efee8a4a4a4d0686a74207e6fa2d47a98122
Tharsan <s.thivyatharsan@gmail.com>
2025-01-28 23:57:46 +0530

    tailwind

--- my-react-app/src/components/DropdownMenu.jsx
+++ my-react-app/src/components/DropdownMenu.jsx
@@ -53,7 +53,7 @@ const DropdownMenu = ({ title, items, header, up, renderItem }) => {
   }, []);
 
   return (
-    <li className="dropdown" ref={menuRef}>
+    <li className="inline block relative text-[#41444B] hover:text-[#FFD700] font-[cinzel] font-medium text-[18px] dropdown" ref={menuRef}>
       <a className="dropdown-toggle">{title}</a>
       <div className={`dropdown-content ${up}`}>
         {header && <h3>{header}</h3>}
