fb8400cccfbebf83154b7ee4af3de9c215641126
Tharsan <s.thivyatharsan@gmail.com>
2025-02-19 23:15:14 +0530

    Razorpay

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,6 @@ import connectCloudinary from './config/cloudinary.js';
 import userRouter from './routes/userRoute.js'
 import productRouter from './routes/productRoute.js'
 import cookieParser from 'cookie-parser';
-import orderRouter from './routes/orderRoute.js'
 import cartRouter from './routes/cartRoutes.js'
 import adminRouter from './routes/adminRoute.js'
 import contactRouter from './routes/contactRoutes.js'
--- backend/routes/orderRoute.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import placeOrder from '../controller/orderController.js';
-
-const orderRouter = express.Router();
-
-orderRouter.post('/placeOrder', placeOrder);
-
-export default orderRouter;
